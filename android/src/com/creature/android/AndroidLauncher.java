6a06af99b0b9629af484b91e465f2da71083b84a
Denis Kniazhev <dkniazhev@spredfast.com>
2015-10-23 22:06:26 +0300

    Added Android module.

---
+++ android/src/com/creature/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.creature.android;
+
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useAccelerometer = false;
+        config.useCompass = false;
+        initialize(new MyGdxGame(), config);
+    }
+}
